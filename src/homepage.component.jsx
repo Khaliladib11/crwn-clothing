929642ad9c64ea61525da94a17defb512a850161
Khalil Adib <Khaliladib11@gmail.com>
2021-08-02 10:59:14 +0300

    Adding Home Page Style

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './homepage.styles.css';
 
 const HomePage = () => {
     return (
@@ -11,35 +11,27 @@ const HomePage = () => {
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className='title'>JACKETS</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-            <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className='title'>WOMEN</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
-            <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className='title'>MEN</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>JACKETS</h1>
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
